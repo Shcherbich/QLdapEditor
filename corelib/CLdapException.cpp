b324047657e2fab0123760c26416350113706a63
AppSys\kabiak <johndoe@example.com>
2019-01-15 12:54:37 +0300

    The match rules algorithm has been added

---
+++ corelib/CLdapException.cpp
@@ -0,0 +1,28 @@
+#include "CLdapException.h"
+
+namespace ldapcore
+{
+
+CLdapNameMissedException::CLdapNameMissedException(char const* lpText)
+    : m_what(lpText)
+{
+}
+
+char const* CLdapNameMissedException::what() const _GLIBCXX_USE_NOEXCEPT
+{
+    return m_what.c_str();
+}
+
+
+CLdapMatchRuleException::CLdapMatchRuleException(char const* lpText)
+	: m_what(lpText)
+{
+}
+
+char const* CLdapMatchRuleException::what() const _GLIBCXX_USE_NOEXCEPT
+{
+	return m_what.c_str();
+}
+
+
+}
--- corelib/CLdapSchema.cpp
+++ corelib/CLdapSchema.cpp
@@ -1,4 +1,6 @@
 #include <map>
+#include <algorithm>
+#include <functional>
 #include <QRegExp>
 #include "CLdapSchema.h"
 #include "LDAPAttribute.h"
@@ -114,6 +116,107 @@ std::tuple<AttrType, bool> CLdapSchema::GetAttributeInfoByName(std::string attrN
     }
     return std::tuple<AttrType, bool>{AttrType::UnknownText, false};
 
+}
+
+
+bool CLdapSchema::isNameExist(std::string attributeName) throw (CLdapNameMissedException)
+{
+    auto f = m_impl->attr2info.find(attributeName);
+    if (f == m_impl->attr2info.end())
+    {
+        QString error = QString("There is no arribute '%1' for this schema").arg(attributeName.c_str());
+        throw CLdapNameMissedException(error.toStdString().c_str());
+    }
+    return true;
+}
+
+using namespace std;
+using namespace std::placeholders;
+
+std::string CheckBitString(std::string value)
+{
+    for (size_t i = 0; i < value.size(); i++)
+    {
+        auto ch = value.at(i);
+        bool b1 = ch == '0';
+        bool b2 = ch == '1';
+        if (!b1 && !b2)
+        {
+            return std::string("The symbol '") + ch + "' is invalid";
+        }
+    }
+    return std::string("");
+}
+
+std::string CheckBool(std::string value)
+{
+    bool b1 = value == "TRUE";
+    bool b2 = value == "FALSE";
+
+    return (b1 || b2) ? std::string("") : std::string("The value ")  + value + "' is invalid";;
+}
+
+std::string CheckCountryStringImpl(std::string value, std::string extendedSet)
+{
+    for (size_t i = 0; i < value.size(); i++)
+    {
+        auto ch = value.at(i);
+        bool b1 = isalpha(ch);
+        bool b2 = isdigit(ch);
+        bool b3 = extendedSet.find(ch) != std::string::npos;
+        if (!b1 && !b2)
+        {
+            return std::string("The symbol '") + ch + "' is invalid";
+        }
+    }
+    return std::string("");
+}
+
+
+std::string CheckOctet(std::string value)
+{
+    static std::string validSet1 =  "abcdef";
+    static std::string validSet2 =  "ABCDEF";
+
+    for (size_t i = 0; i < value.size(); i++)
+    {
+        auto ch = value.at(i);
+        bool b1 = isdigit(ch);
+        bool b2 = validSet1.find(ch) != std::string::npos;
+        bool b3 = validSet2.find(ch) != std::string::npos;
+        if (!b1 && !b2 && !b3)
+        {
+            return std::string("The symbol '") + ch + "' is invalid";
+        }
+    }
+    return std::string("");
+}
+
+static std::string validSetOfCountry  = "'()+ -.,/:?";
+static std::string validSetOfCountry2 = "'()+ -.,/:?$";
+
+std::map<std::string, std::function< std::string(std::string)> > syntaxNumber2toCheckFunction
+{
+    {"1.3.6.1.4.1.1466.115.121.1.6", CheckBitString},
+    {"1.3.6.1.4.1.1466.115.121.1.7", CheckBool},
+    {"1.3.6.1.4.1.1466.115.121.1.11", std::bind(CheckCountryStringImpl, _1, validSetOfCountry)},
+    {"1.3.6.1.4.1.1466.115.121.1.14", CheckOctet},
+    {"1.3.6.1.4.1.1466.115.121.1.15", CheckOctet},
+    {"1.3.6.1.4.1.1466.115.121.1.22", std::bind(CheckCountryStringImpl, _1, validSetOfCountry2)},
+};
+
+
+void CLdapSchema::checkBySyntaxName(std::string attributeName, std::string value) throw (CLdapMatchRuleException)
+{
+    auto f = syntaxNumber2toCheckFunction.find(attributeName);
+    if (f != syntaxNumber2toCheckFunction.end())
+    {
+        auto checkReturn = f->second(value);
+        if (checkReturn.size() != 0)
+        {
+            throw CLdapMatchRuleException(checkReturn.c_str());
+        }
+    }
 }
 
 }
