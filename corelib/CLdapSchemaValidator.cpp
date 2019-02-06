6748dba565c9c9d30f55967871fd0cfce20df005
AppSys\kabiak <johndoe@example.com>
2019-02-06 09:24:22 +0300

    CLdapSchema has been added source file for only validation functions

---
+++ corelib/CLdapSchemaValidator.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <regex>
+#include <tuple>
+#include "LDAPSchema.h"
+
+
+using uEntry = std::shared_ptr<LDAPEntry>;
+
+namespace ldapcore
+{
+
+
+}
+
