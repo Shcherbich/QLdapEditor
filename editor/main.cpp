19e5b8281e5b26308a253dfabc923818365cd6e3
AppSys\kabiak <johndoe@example.com>
2019-02-12 08:02:42 +0300

    Automatic/manual reconnect #18

--- editor/main.cpp
+++ editor/main.cpp
@@ -2,27 +2,96 @@
 #include "connectiondialog.h"
 #include "ldapsettings.h"
 #include "CLdapData.h"
-
 #include <QApplication>
 #include <QSettings>
 #include <signal.h>
+#include <unistd.h>
+#include <cerrno>
+#include <system_error>
+#include <iostream>
 
-int main(int argc, char *argv[])
+static sigset_t theMask;
+static int count = 0;
+
+static void signalWrapper(int theSignalNumber, siginfo_t* theSignalDescription, void* theUserContext)
 {
-    // Do by default to ignore this interrupt
-    ::signal(SIGPIPE, SIG_IGN);
+	// Do something here as reaction to you SIGPIPE
+	// This is better way to react on such things
 
-    QApplication a(argc, argv);
+	if (SIGPIPE == theSignalNumber)
+	{
+		std::cout << "Got signal SIGPIPE" << std::endl;
+	}
+	else
+	{
+		std::cout << "Got signal " << theSignalNumber << std::endl;
+	}
 
-    ldapeditor::CLdapSettings settings;
-    ldapcore::CLdapData ldapData;
-    ldapeditor::CConnectionDialog connectionDlg(settings, ldapData);
-    if(connectionDlg.exec() == QDialog::Rejected)
-    {
-        return 0;
-    }
 
-    ldapeditor::MainWindow w(settings, ldapData);
-    w.show();
-    return a.exec();
+	// Reinstall handler
+	struct ::sigaction sa;
+	sa.sa_sigaction = &signalWrapper;
+	sa.sa_mask = theMask;
+	sa.sa_flags = SA_SIGINFO;
+
+	try
+	{
+		if (::sigaction(theSignalNumber, &sa, NULL) == -1)
+		{
+			//throw std::error_code(errno, std::system_category());
+		}
+	}
+	catch (const std::error_code& ec)
+	{
+		std::cerr << ec << std::endl;
+	}
+
+	count++;
+}
+
+void setupSignalHandlers()
+{
+	struct ::sigaction sa;
+
+	// Prepare mask
+	sigemptyset(&theMask);
+	sigaddset(&theMask, SIGPIPE);
+	// Add some more if you need it to process
+
+	sa.sa_mask      = theMask;
+	sa.sa_flags     = SA_SIGINFO;
+	sa.sa_sigaction = &signalWrapper;
+
+	// Perform setup
+	try
+	{
+		if (::sigaction(SIGPIPE, &sa, NULL) == -1)
+		{
+			//throw std::error_code(errno, std::system_category());
+		}
+	}
+	catch (const std::error_code& ec)
+	{
+		std::cerr << ec << std::endl;
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	setupSignalHandlers();
+
+	QApplication a(argc, argv);
+
+	ldapeditor::CLdapSettings settings;
+	ldapcore::CLdapData ldapData;
+	ldapeditor::CConnectionDialog connectionDlg(settings, ldapData);
+	if (connectionDlg.exec() == QDialog::Rejected)
+	{
+		return 0;
+	}
+
+	ldapeditor::MainWindow w(settings, ldapData);
+	w.show();
+	return a.exec();
 }
